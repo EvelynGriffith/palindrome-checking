438288f0156be4892de0ddc25444f01e5545d736
EvelynGriffith <70599987+EvelynGriffith@users.noreply.github.com>
2021-10-14 23:01:44 -0400

    fixing test case for util

--- palindromechecker/tests/test_util.py
+++ palindromechecker/tests/test_util.py
@@ -16,7 +16,7 @@ reload(util)
 def test_human_readable_boolean_true():
     """Ensure that a human-readable true boolean works correctly."""
     true_value = True
-    true_value_human_readable = util.get_human_readable_boolean(true_value)
+    true_value_human_readable = util.human_readable_boolean(true_value)
     assert true_value_human_readable == "Yes, it is!"
 
 
@@ -24,5 +24,5 @@ def test_human_readable_boolean_false():
     """Ensure that a human-readable false boolean works correctly."""
     # add a test case that follows the provided example
     false_value = False
-    false_value_human_readable = util.get_human_readable_boolean(false_value)
+    false_value_human_readable = util.human_readable_boolean(false_value)
     assert false_value_human_readable == "No, it is not!"
